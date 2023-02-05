5730f13b1833ee9d70d39b18c215eee1d30701e4
Mitch Ludkey <mludk92@hotmail.com>
2023-02-05 16:47:23 -0600

    built the show collect function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,5 @@
 console.log('***** Music Collection *****')
-let collection = [
-    {title: "Unplugged",
-    artist: "Eric Clapton",
-    yearPublished: 1992
-}
-]
+let collection = []
 
 function addToCollection(titleOfAlbum,artistOfAlbum,yearPublishedOfAlbum){
     let newAlbum = {
@@ -22,5 +17,21 @@ addToCollection('Nightmare','Avenged Sevenfold',2010)
 addToCollection('Black Rain','Ozzy Osbourne',2007)
 addToCollection('The Resistance','Muse',2009)
 addToCollection('Keep It Going','Mad Caddies',2007)
-addToCollection('Center:Level:Roar','Young Blood Brass Band',2003)
-console.table(collection)
+addToCollection('Unplugged','Eric Clapton',1992)
+console.log(collection)
+
+
+
+// - Console.log the number of items in the array.
+// - Loop over the array and console.log each album's
+//  information formatted like: `TITLE by ARTIST,
+//   published in YEAR`.
+function showCollection(arrayOfObjects){
+    console.log(arrayOfObjects.lenght)
+    for(let i in arrayOfObjects){
+     console.log(`${arrayOfObjects[i].title} by ${arrayOfObjects[i].artist}, published in ${arrayOfObjects[i].yearPublished}.`)
+    }
+}
+
+console.log(collection.length)
+console.log(collection[0].title)
