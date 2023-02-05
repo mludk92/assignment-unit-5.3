9e246d931a51a55c8617f52be96a151788798d92
Mitch Ludkey <mludk92@hotmail.com>
2023-02-05 16:13:55 -0600

    built funciton to add to array of objects, added first song

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,20 @@
 console.log('***** Music Collection *****')
-let collection = []
+let collection = [
+    {title: "Unplugged",
+    artist: "Eric Clapton",
+    yearPublished: 1992
+}
+]
+
+function addToCollection(titleOfAlbum,artistOfAlbum,yearPublishedOfAlbum){
+    let newAlbum = {
+    "title": titleOfAlbum,
+    "artist": artistOfAlbum,
+    "yearPublished": yearPublishedOfAlbum
+    }
+    collection.push(newAlbum)
+}
+
+console.log(collection)
+addToCollection('Popestar','Ghost',2016)
+console.log(collection)
